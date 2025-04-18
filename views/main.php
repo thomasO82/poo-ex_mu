0caa9bc352cb9af4b62dcf9b96141bf24d5f23d1
o-thomas <thomas.oliver@ri7.fr>
2025-04-18 14:53:09 +0200

    dfdfdf

--- views/main.php
+++ views/main.php
@@ -8,40 +8,39 @@
 </head>
 
 <body>
+
     <h1>Super poke</h1>
     <section>
         <h2>Capturer poke</h2>
         <?php if ($poke): ?>
             <p>un pokemon sauvage apparait, <?= $poke->getName() ?> est dans la place</p>
-        <?php else: ?>
-            <p>Tout les pokemon ont ete capturer</p>
-        <?php endif ?>
-
-
-
-        <form action="/capture" method="post">
+            <form action="/capture" method="post">
             <button type="submit" name="capture" value=<?= $poke->getId() ?>>Capturer</button>
         </form>
 
         <form action="/free" method="POST">
             <button type="submit" name="chill">Laisser tranquile</button>
         </form>
+        <?php else: ?>
+            <p>Tout les pokemon ont ete capturer</p>
+        <?php endif ?>
+
+
+
+      
     </section>
 
     <section>
         <h2>Mes pokemons</h2>
-        <?php if ($poke): ?>
-            <?php foreach ($pokesCaptured as $pokeCapt): ?>
+      
+        <?php foreach ($pokesCaptured as $pokeCapt): ?>
 
-                <h3><?= $pokeCapt['name'] ?></h3>
-                <form action="" method="post">
-                    <button type="submit" name="free" value=<?= $pokeCapt['id'] ?>>Liberer</button>
-                </form>
+            <h3><?= $pokeCapt['name'] ?></h3>
+            <form action="" method="post">
+                <button type="submit" name="free" value=<?= $pokeCapt['id'] ?>>Liberer</button>
+            </form>
 
-            <?php endforeach ?>
-        <?php else : ?>
-            <p>Tout les pokemons ont ete capturé</p>
-        <?php endif ?>
+        <?php endforeach ?>
     </section>
 </body>
 
